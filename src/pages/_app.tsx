27f76e8de5b094408ac614cc39cab754b2294f6b
JacobParis <jacob@jacobpariseau.com>
2021-09-07 20:47:40 +0200

    Add developer toolbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,33 @@
 import '../styles/globals.scss'
+import 'devtoolbar-react/lib/index.css'
 
 import React from 'react'
 
 import {DefaultSeo} from 'next-seo'
 import {ThemeProvider} from 'next-themes'
 import {AppProps} from 'next/app'
+import dynamic from 'next/dynamic'
 
 import SEO from '../../next-seo.json'
+
+const DynamicComponentWithNoSSR = dynamic(
+  async () => (await import('devtoolbar-react/lib')).DevToolbar,
+  {ssr: false},
+)
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
       <ThemeProvider forcedTheme={Component['theme'] || null}>
-          <Component {...pageProps} />
+        <DynamicComponentWithNoSSR
+          hosts={{
+            production: ['https://www.threeq.app'],
+            staging: ['https://standup-lovat.vercel.app'],
+            development: ['http://localhost:3050', 'http://localhost:3051'],
+          }}
+        />
+        <Component {...pageProps} />
       </ThemeProvider>
     </>
   )
